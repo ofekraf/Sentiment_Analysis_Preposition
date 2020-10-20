44411c1d2a6b677e17007d448d8410161023fc1c
Ofekraf <Ofek.Rafaeli@mail.huji.ac.il>
2020-10-20 17:42:13 +0300

    added raw_sentences and their respective parsing into a readable format

--- analysis.py
+++ analysis.py
@@ -1,11 +1,12 @@
 import nltk
 from textblob import TextBlob
 
+EXTRACT_PREPOSITION_SENTENCES = False
+RUN_ANALYSIS = False
+CHECKED_PREPOSITIONS = ["but"]
+
+
 # todo: Flair, Textblob
-
-
-
-def
 
 
 def get_sid():
@@ -13,16 +14,58 @@ def get_sid():
     from nltk.sentiment.vader import SentimentIntensityAnalyzer
     return SentimentIntensityAnalyzer()
 
-def main():
-    nltk_vader = get_sid()
-    sentences = ["this is a good sentence", "this is a bad sentence"]
 
-    analysis = []
-    for sentence in sentences:
-        analysis.append(nltk_vader.polarity_scores(sentence))
-    print(analysis)
+def get_substring_propositions():
+    substrings = []
+    for prop in CHECKED_PREPOSITIONS:
+        substrings.append(" " + prop + " ")
+        substrings.append("," + prop + " ")
+        substrings.append(" " + prop + ",")
+        substrings.append("," + prop + ",")
+        substrings.append("." + prop + " ")
+    return substrings
+
+
+def extract_preposition_sentences():
+    preposition_split_sentences = {prep: [] for prep in CHECKED_PREPOSITIONS}
+    parse_raw_sentences(preposition_split_sentences)
+
+    for prep in preposition_split_sentences:
+        with open(prep + "_sentences", 'w') as writer:
+            for tup in preposition_split_sentences[prep]:
+                writer.write(str(tup) + "\n")
+
+
+def parse_raw_sentences(preposition_split_sentences):
+    with open("RAW_SENTENCES", 'r') as reader:
+        lines = reader.readlines()
+        for line in lines:
+            if '\t' in line:
+                line = line[:line.index('\t')]
+            for preposition in get_substring_propositions():
+                if preposition in line.lower():
+                    splited_line = tuple(line.split(preposition))
+                    if len(splited_line) > 2:
+                        print(line)
+                        continue
+                        # in the corpus of 3000 sentences, this has not occured once:
+                    preposition_split_sentences[preposition[1:-1]].append(
+                        splited_line)
+
+
+def main():
+    if EXTRACT_PREPOSITION_SENTENCES:
+        extract_preposition_sentences()
+
+    if RUN_ANALYSIS:
+        nltk_vader = get_sid()
+        sentences = ["this is a good sentence", "this is a bad sentence"]
+
+        analysis = []
+        for sentence in sentences:
+            analysis.append(nltk_vader.polarity_scores(sentence))
+        print(analysis)
+
 
 if __name__ == '__main__':
     main()
-
-
