ce30a59d8ba46705ef89a139190859ba1c71b894
Ofekraf <Ofek.Rafaeli@mail.huji.ac.il>
2020-10-26 20:02:04 +0200

    added however_file

--- analyzer.py
+++ analyzer.py
@@ -1,7 +1,9 @@
 import nltk
 import matplotlib.pyplot as plt
-# todo: Flair, Textblob
+# todo: Flair
 # todo - parsers: https://elitedatascience.com/python-nlp-libraries
+
+FIRST_TIME = False
 
 class Analyzer:
     FUNCTION_POS = 0
@@ -18,7 +20,8 @@ class Analyzer:
 
     @classmethod
     def _get_sid(cls):
-        nltk.download('vader_lexicon')
+        if FIRST_TIME:
+            nltk.download('vader_lexicon')
         from nltk.sentiment.vader import SentimentIntensityAnalyzer
         return SentimentIntensityAnalyzer()
 
@@ -39,37 +42,35 @@ class Analyzer:
             print("Warning! you must first analyze before plotting! ")
             return
 
-        print("starting plotting")
-
         self.list_of_files = ["but_sentences", "however_sentences"]
 
         fig, all_plots = plt.subplots(len(self.list_of_files), len(self.sentiment_modules))
-        x = [1,2,3]
-        y = [1,2,3]
-        all_plots[0, 0].plot(x, y)
-        all_plots[0, 0].set_title('Axis [0,0]')
-        all_plots[0, 1].plot(x, y, 'tab:orange')
-        all_plots[0, 1].set_title('Axis [0,1]')
-        # for mod_idx, module in enumerate(self.sentiment_modules):
-        #     for prop_idx, file_name in enumerate(self.list_of_files):
-        #         all_plots[mod_idx][prop_idx]
+
+        # all_plots[0, 0].set_title('Axis [0,0]') #todo
+        # all_plots[0, 1].plot(x, y, 'tab:orange')
+        # all_plots[0, 1].set_title('Axis [0,1]')
+
+        for mod_idx, module in enumerate(self.sentiment_modules):
+            for prop_idx, file_name in enumerate(self.list_of_files):
+                all_plots[mod_idx, prop_idx].scatter(self.coords[file_name][module][0], self.coords[file_name][module][1])
+                all_plots[mod_idx, prop_idx].axes.grid = True
 
         fig.show()
 
 
     def analyze(self):
-        # for file in self.list_of_files:
-        file = "but_sentences" #todo
-        self.coords[file] = ([], [])
-        with open(file, 'r') as reader:
-            for line in reader:
-                first, sec = line.split(self.delimiter)
+        self.list_of_files = ["but_sentences","however_sentences"] #todo
+        for file in self.list_of_files:
+            self.coords[file] = {n:([],[]) for n in self.sentiment_modules.keys()}
+            with open(file, 'r') as reader:
+                for line in reader:
+                    first, sec = line.split(self.delimiter)
 
-                first = self.clean(first)
-                sec = self.clean(sec)
-                for _, module in self.sentiment_modules.items():
-                    self.coords[file][0].append(self.calc_score(module, first))
-                    self.coords[file][1].append(self.calc_score(module, sec))
+                    first = self.clean(first)
+                    sec = self.clean(sec)
+                    for mod_name, module in self.sentiment_modules.items():
+                        self.coords[file][mod_name][0].append(self.calc_score(module, first))
+                        self.coords[file][mod_name][1].append(self.calc_score(module, sec))
         self._analysis_done = True
 
     @classmethod
@@ -78,5 +79,10 @@ class Analyzer:
 
     def initialize_modules(self):
         nltk_vader = self._get_sid()
-        self.sentiment_modules[nltk_vader] = (
+        self.sentiment_modules["nltk"] = (
         nltk_vader.polarity_scores, 'compound', lambda x: x*5)
+
+        from textblob import TextBlob
+        self.sentiment_modules["TextBlob"] = (lambda x:TextBlob(x).sentiment,0,lambda x: x*5)
+
+
